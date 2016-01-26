ee017e35a80c4b131deed9d07973a2e1fe8aacae
Flavio Moreira <fhnmor21@gmail.com>
2016-01-26 00:32:44 +0000

    simplified queue

--- utils_containers.hpp
+++ utils_containers.hpp
@@ -19,81 +19,28 @@ namespace Utils
 
 using cSize = u32;
 
-// forward declarations
-template < typename ElementType, cSize Capacity >
-struct FixedDeQueue;
-
-template < typename ElementType, cSize Capacity >
-ErrorCode push_back(FixedDeQueue< ElementType, Capacity >& container, const ElementType& el);
-
-template < typename ElementType, cSize Capacity >
-ErrorCode push_front(FixedDeQueue< ElementType, Capacity >& container, const ElementType& el);
-
-template < typename ElementType, cSize Capacity >
-ElementType pop_back(FixedDeQueue< ElementType, Capacity >& container, ElementType fallback);
-
-template < typename ElementType, cSize Capacity >
-ElementType pop_front(FixedDeQueue< ElementType, Capacity >& container, ElementType fallback);
-
-template < typename ElementType, cSize Capacity >
-ElementType* front(FixedDeQueue< ElementType, Capacity >& container);
-
-template < typename ElementType, cSize Capacity >
-ElementType* back(FixedDeQueue< ElementType, Capacity >& container);
-
-template < typename ElementType, cSize Capacity >
-ElementType* at(FixedDeQueue< ElementType, Capacity >& container, cSize pos);
-
-template < typename ElementType, cSize Capacity >
-ErrorCode del(FixedDeQueue< ElementType, Capacity >& container, cSize pos);
-
-template < typename ElementType, cSize Capacity >
-cSize length(FixedDeQueue< ElementType, Capacity >& container);
-
 // declarations
 template < typename ElementType, cSize Capacity >
-struct FixedDeQueue
+struct FixedDEQueue
 {
   using Type = ElementType;
   const cSize maxLength = Capacity;
 
-  FixedDeQueue() = delete;
-  explicit FixedDeQueue(const ElementType& init);
-  FixedDeQueue(const FixedDeQueue& other);
-  FixedDeQueue& operator=(const FixedDeQueue& other);
+  FixedDEQueue() = delete;
+  explicit FixedDEQueue(const ElementType& init);
+  FixedDEQueue(const FixedDEQueue& other);
+  FixedDEQueue& operator=(const FixedDEQueue& other);
 
-private:
   Type array_[Capacity];
   cSize length_;
   cSize internalLength_;
   cSize firstPos_;
   cSize lastPos_;
-
-  friend ErrorCode push_back< ElementType, Capacity >(FixedDeQueue& container,
-                                                      const ElementType& el);
-
-  friend ErrorCode push_front< ElementType, Capacity >(FixedDeQueue& container,
-                                                       const ElementType& el);
-
-  friend ElementType pop_back< ElementType, Capacity >(FixedDeQueue& container,
-                                                       ElementType fallback);
-
-  friend ElementType pop_front< ElementType, Capacity >(FixedDeQueue& container,
-                                                        ElementType fallback);
-
-  friend ElementType* front< ElementType, Capacity >(FixedDeQueue& container);
-
-  friend ElementType* back< ElementType, Capacity >(FixedDeQueue& container);
-
-  friend ElementType* at< ElementType, Capacity >(FixedDeQueue& container, cSize pos);
-
-  friend ErrorCode del< ElementType, Capacity >(FixedDeQueue& container, cSize pos);
-
-  friend cSize length< ElementType, Capacity >(FixedDeQueue& container);
 };
 
+// implents the constructors
 template < typename ElementType, cSize Capacity >
-FixedDeQueue< ElementType, Capacity >::FixedDeQueue(const ElementType& init)
+FixedDEQueue< ElementType, Capacity >::FixedDEQueue(const ElementType& init)
     : array_{}
     , length_{0}
     , internalLength_{0}
@@ -104,7 +51,7 @@ FixedDeQueue< ElementType, Capacity >::FixedDeQueue(const ElementType& init)
 }
 
 template < typename ElementType, cSize Capacity >
-FixedDeQueue< ElementType, Capacity >::FixedDeQueue(const FixedDeQueue& other)
+FixedDEQueue< ElementType, Capacity >::FixedDEQueue(const FixedDEQueue& other)
     : array_{}
     , length_{other.length}
     , internalLength_{other.nternalLentgth}
@@ -115,8 +62,8 @@ FixedDeQueue< ElementType, Capacity >::FixedDeQueue(const FixedDeQueue& other)
 }
 
 template < typename ElementType, cSize Capacity >
-FixedDeQueue< ElementType, Capacity >& FixedDeQueue< ElementType, Capacity >::
-operator=(const FixedDeQueue& other)
+FixedDEQueue< ElementType, Capacity >& FixedDEQueue< ElementType, Capacity >::
+operator=(const FixedDEQueue& other)
 {
   length_ = other.length;
   internalLength_ = other.internalLentgth;
@@ -124,6 +71,111 @@ operator=(const FixedDeQueue& other)
   lastPos_ = other.lastPos;
   std::copy(other.array_, other.array_ + other.internalLength, array_);
   return *this;
+}
+
+// accessor methods
+
+template < typename ElementType, cSize Capacity >
+ErrorCode push_back(FixedDEQueue< ElementType, Capacity >& container, const ElementType& el)
+{
+  if(container.length_ < container.maxLength)
+  {
+    container.lastPos_ = (container.lastPos_ + 1) % container.maxLength;
+    container.array_[container.lastPos_] = el;
+    if(container.length_ == 0)
+    {
+      container.firstPos_ = container.lastPos_;
+    }
+    ++container.length_;
+    return NO_ERROR;
+  }
+  return UNKNOWN_ERROR;
+}
+
+template < typename ElementType, cSize Capacity >
+ErrorCode push_front(FixedDEQueue< ElementType, Capacity >& container, const ElementType& el)
+{
+  if(container.length_ < container.maxLength)
+  {
+    container.firstPos_ = (container.firstPos_ - 1 + container.maxLength) % container.maxLength;
+    container.array_[container.firstPos_] = el;
+    if(container.length_ == 0)
+    {
+      container.lastPos_ = container.firstPos_;
+    }
+    ++container.length_;
+    return NO_ERROR;
+  }
+  return UNKNOWN_ERROR;
+}
+
+template < typename ElementType, cSize Capacity >
+ElementType pop_back(FixedDEQueue< ElementType, Capacity >& container, ElementType fallback)
+{
+  if(container.length_ > 0)
+  {
+    --container.length_;
+    --container.lastPos_;
+    return container.array_[container.lastPos_ + 1];
+  }
+  return fallback;
+}
+
+template < typename ElementType, cSize Capacity >
+ElementType pop_front(FixedDEQueue< ElementType, Capacity >& container, ElementType fallback)
+{
+  if(container.length_ > 0)
+  {
+    --container.length_;
+    ++container.firstPos_;
+    return container.array_[container.firstPos_ - 1];
+  }
+  return fallback;
+}
+
+template < typename ElementType, cSize Capacity >
+ElementType* front(FixedDEQueue< ElementType, Capacity >& container)
+{
+  if(container.length_ > 0)
+  {
+    return &(container.array_[container.firstPos_]);
+  }
+  return nullptr;
+}
+
+template < typename ElementType, cSize Capacity >
+ElementType* back(FixedDEQueue< ElementType, Capacity >& container)
+{
+  if(container.length_ > 0)
+  {
+    return &(container.array_[container.lastPos_]);
+  }
+  return nullptr;
+}
+
+template < typename ElementType, cSize Capacity >
+ElementType* at(FixedDEQueue< ElementType, Capacity >& container, cSize pos)
+{
+  if(pos < container.length_)
+  {
+    cSize index = (pos + container.firstPos_) % container.maxLength;
+    return &(container.array_[index]);
+  }
+  return nullptr;
+}
+
+template < typename ElementType, cSize Capacity >
+void clear(FixedDEQueue< ElementType, Capacity >& container)
+{
+  container.length_ = 0;
+  container.firstPos_ = Capacity >> 1;
+  container.lastPos_ = container.firstPos_;
+}
+
+template < typename ElementType, cSize Capacity >
+cSize length(FixedDEQueue< ElementType, Capacity >& container)
+{
+  return container.length_;
 }
 
 } // end namespace Utils
